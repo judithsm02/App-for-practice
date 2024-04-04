1603380d7017e00cbd8ac96529b9f049f8c72707
judith Sanchez <judith.sanchez.marty@gmail.com>
2024-04-04 08:30:19 +0000

    Initial Commit @4/4/2024, 8:30:19 AM

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["practiceapp/practicej/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"practice_app/practicej/test/unit/controller/Carrier.controller"
+], function () {
+	"use strict";
+});
