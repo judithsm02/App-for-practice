fd503d6f122ed221b6c053689a8f7c3e06e67ca0
Judith Sanchez <judith.sanchez-marty.external@capgemini.com>
2024-05-24 08:45:39 +0000

    Wizard this steps

--- webapp/controller/Wizard.controller.js
+++ webapp/controller/Wizard.controller.js
@@ -1,20 +1,27 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
+  [
+      "sap/ui/core/UIComponent",
+      'sap/ui/core/mvc/Controller',
+      'sap/ui/model/odata/v2/ODataModel'
+
+  ],
+  function(BaseController, controller,UIComponent,ODataModel) {
+    "use strict";
+
+    return BaseController.extend("practiceapp.practicej.controller.Wizard", {
+      onInit: function() {
+
         
-    ],
-     /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function(Controller) {
-      "use strict";
-  
-      return Controller.extend("practiceapp.practicej.controller.Wizard", {
-        onInit: function() {
+        
+        
+      },
+    
+
+    
+
+    
 
 
-        }
-      });
-    }
-  );
-  
+    });
+  }
+);
